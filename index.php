582a57ff44b8bd0da71f4f30c5705baa87c72f29
jorisgengler <jorisgengler@hotmail.com>
2017-12-27 15:00:53 +0100

    Grid with query

---
+++ index.php
@@ -0,0 +1,94 @@
+<html>
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CSS-Grid</title>
+
+  <!-- CSS -->
+  <link rel="STYLESHEET" href="main.css" type="text/css">
+
+  <!-- javascript-->
+</head>
+
+<body>
+	<div class="grid">
+
+		<div class ="section1">
+			<center><h1>Head 1</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+			</div>
+		</div>
+
+		<div class ="section2">
+			<center><h1>Head 2</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+				</div>
+		</div>
+
+		<div class ="section3">
+			<center><h1>Head 3</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+			</div>
+		</div>
+
+		<div class ="section4">
+			<center><h1>Head 4</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+				</div>
+			</div>
+
+		<div class ="section5">
+			<center><h1>Head 5</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+				</div>
+			</div>
+
+		<div class ="section6">
+			<center><h1>Head 6</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+				</div>
+			</div>
+
+		<div class ="section7">
+			<center><h1>Head 7</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+				</div>
+			</div>
+
+		<div class ="section8">
+			<center><h1>Head 8</h1></center></br>
+			<div class="textvlak">
+				<p>
+			Nullam vel suscipit lectus. Phasellus felis sem, semper a rutrum quis, consequat at leo. Nulla imperdiet neque eu ante ultricies sollicitudin non at sem. Praesent vehicula luctus libero non convallis. Donec eu mauris nisl. Nullam efficitur libero nec lorem vestibulum, ut sagittis odio commodo. Praesent accumsan nisl dolor, eget finibus augue fringilla eget. Nam egestas bibendum ornare. Quisque quam turpis, gravida vel risus at, aliquet viverra purus. Pellentesque pulvinar tortor a quam bibendum, quis mattis metus lobortis. Curabitur lacus ligula, semper vitae finibus eu, dictum a arcu. Pellentesque placerat, orci nec rutrum condimentum, arcu nisl pellentesque tortor, id auctor eros ex eget lorem. Morbi ut nulla id nisl elementum consectetur eget at felis.
+				</p>
+				</div>
+			</div>
+
+
+	</div>
+
+</body>
+
+</html>
